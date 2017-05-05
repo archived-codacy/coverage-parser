3dc318cb62feef70add2637ea74dc696260f91ba
Daniel Reigada <daniel@codacy.com>
2017-05-05 15:34:34 +0100

    Do not filter languages with unknown extensions

--- src/main/scala/com/codacy/parsers/util/LanguageUtils.scala
+++ src/main/scala/com/codacy/parsers/util/LanguageUtils.scala
@@ -4,8 +4,8 @@ import com.codacy.api.Language
 
 object LanguageUtils {
 
-  def getExtension(language: Language.Value): String = {
-    language match {
+  def getExtension(language: Language.Value): Option[String] = {
+    Option(language).collect {
       case Language.Java => ".java"
       case Language.Python => ".py"
       case Language.Scala => ".scala"
