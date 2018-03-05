514771cf9ce79542dfaa0003c0c772b3fe7060a3
Daniel Reigada <daniel@codacy.com>
2018-03-05 18:45:38 +0000

    Update codacy scala api

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
 
-  val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.3"
+  val codacyScalaApi = "com.codacy" %% "codacy-api-scala" % "3.0.4"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4" % "test"
 
 }
