e8be85b0612b3cc6a6a878b33746ecb7c294373f
Francisco <francisco@codacy.com>
2020-01-28 17:13:55 +0000

    style: fix CloverParserTest style to match scalafmt

--- src/test/scala/com/codacy/parsers/CloverParserTest.scala
+++ src/test/scala/com/codacy/parsers/CloverParserTest.scala
@@ -9,25 +9,18 @@ class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers wit
   "CloverParser" should {
     "identify if report is invalid" in {
       // use some coverage file that does not follow the Clover format
-      val reader = CloverParser.parse(
-        new File("."),
-        new File("src/test/resources/test_cobertura.xml"))
+      val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_cobertura.xml"))
 
       reader.isLeft shouldBe true
     }
 
     "identify if report is valid" in {
-      val reader = CloverParser.parse(
-        new File("."),
-        new File("src/test/resources/test_clover.xml"))
+      val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_clover.xml"))
       reader.isRight shouldBe true
     }
 
     "return a valid report" in {
-      val reader = CloverParser.parse(
-        new File("/home/codacy-php/"),
-        new File("src/test/resources/test_clover.xml"))
-
+      val reader = CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml"))
 
       val report = reader.right.value
       report.total shouldBe 38
@@ -43,18 +36,28 @@ class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers wit
       val secondFileReport = report.fileReports(1)
       secondFileReport.total shouldBe 33
       secondFileReport.filename shouldBe "src/Codacy/Coverage/Report/CoverageReport.php"
-      secondFileReport.coverage shouldBe Map(11 -> 1, 12 -> 1, 13 -> 1, 16 -> 1,
-      19 -> 0, 30 -> 0, 31 -> 0, 32 -> 0, 33 -> 0, 36 -> 0, 39 -> 0, 42 -> 0)
+      secondFileReport.coverage shouldBe Map(
+        11 -> 1,
+        12 -> 1,
+        13 -> 1,
+        16 -> 1,
+        19 -> 0,
+        30 -> 0,
+        31 -> 0,
+        32 -> 0,
+        33 -> 0,
+        36 -> 0,
+        39 -> 0,
+        42 -> 0
+      )
     }
 
     "return a valid report even without packages" in {
-      val readerWithoutPackages = CloverParser.parse(
-        new File("/home/codacy-php/"),
-        new File("src/test/resources/test_clover_without_packages.xml"))
+      val readerWithoutPackages = CloverParser
+        .parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover_without_packages.xml"))
 
-      val readerWithPackages = CloverParser.parse(
-        new File("/home/codacy-php/"),
-        new File("src/test/resources/test_clover.xml"))
+      val readerWithPackages =
+        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml"))
 
       readerWithoutPackages shouldBe readerWithPackages
     }
