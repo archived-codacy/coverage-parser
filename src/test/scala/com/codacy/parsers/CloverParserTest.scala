b48b288efc3ce8207b0e4ad05dca3b06992c56ed
Francisco <francisco@codacy.com>
2020-01-29 17:01:51 +0000

    test: Split tests to perform more contained validations

--- src/test/scala/com/codacy/parsers/CloverParserTest.scala
+++ src/test/scala/com/codacy/parsers/CloverParserTest.scala
@@ -6,37 +6,86 @@ import com.codacy.parsers.implementation.CloverParser
 import org.scalatest.{BeforeAndAfterAll, EitherValues, Matchers, WordSpec}
 
 class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers with EitherValues {
-  "CloverParser" should {
-    "identify if report is invalid" in {
-      // use some coverage file that does not follow the Clover format
-      val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_cobertura.xml"))
+  "parse" should {
+    "identify report as invalid" when {
+      "file does not follow the Cobertura format" in {
+        // use some coverage file that does not follow the Clover format
+        val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_cobertura.xml"))
 
-      reader.isLeft shouldBe true
+        reader shouldBe 'left
+      }
+
+      "file does not exist" in {
+        val reader = CloverParser.parse(new File("."), new File("src/test/resources/non-existent.xml"))
+
+        reader shouldBe 'left
+      }
     }
 
-    "identify if report is valid" in {
-      val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_clover.xml"))
-      reader.isRight shouldBe true
+    "identify the report as valid" when {
+      "report has packages" in {
+        val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_clover.xml"))
+        reader shouldBe 'right
+      }
+
+      "report does not have packages" in {
+        val readerWithoutPackages = CloverParser
+          .parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover_without_packages.xml"))
+
+        readerWithoutPackages shouldBe 'right
+      }
     }
 
-    "return a valid report" in {
-      val reader = CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml"))
+    "return the same report with or without packages" in {
+      val readerWithoutPackages = CloverParser
+        .parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover_without_packages.xml"))
 
-      val report = reader.right.value
+      val readerWithPackages =
+        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml"))
+
+      readerWithoutPackages shouldBe readerWithPackages
+    }
+
+    "return a report with the expected number of files" in {
+      val report =
+        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+      report.fileReports should have length 5
+    }
+
+    "return a report with the expected total coverage" in {
+      val report =
+        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
       report.total shouldBe 38
-      // 5 files used in the initial test
-      report.fileReports.length shouldBe 5
+    }
 
+    "return a report with the expected file names" in {
+      val report =
+        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+      report.fileReports.map(_.filename).sorted shouldBe Seq(
+        "src/Codacy/Coverage/Parser/Parser.php",
+        "src/Codacy/Coverage/Report/CoverageReport.php",
+        "vendor/sebastian/global-state/src/Blacklist.php",
+        "vendor/sebastian/global-state/src/Restorer.php",
+        "vendor/sebastian/global-state/src/Snapshot.php"
+      ).sorted
+    }
+
+    "return a report with the expected file coverage" in {
+      val report =
+        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
       // coverage percentage for file Parser.php
       val firstFileReport = report.fileReports.headOption.getOrElse(fail("file reports list is empty"))
       firstFileReport.total shouldBe 33
-      firstFileReport.filename shouldBe "src/Codacy/Coverage/Parser/Parser.php"
 
-      // check only the CoverageReport.php
+      // coverage percentage for file Parser.php
       val secondFileReport = report.fileReports(1)
       secondFileReport.total shouldBe 33
-      secondFileReport.filename shouldBe "src/Codacy/Coverage/Report/CoverageReport.php"
-      secondFileReport.coverage shouldBe Map(
+    }
+
+    "return a report with the expected file line coverage" in {
+      val report =
+        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+      report.fileReports(1).coverage shouldBe Map(
         11 -> 1,
         12 -> 1,
         13 -> 1,
@@ -50,16 +99,6 @@ class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers wit
         39 -> 0,
         42 -> 0
       )
-    }
-
-    "return a valid report even without packages" in {
-      val readerWithoutPackages = CloverParser
-        .parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover_without_packages.xml"))
-
-      val readerWithPackages =
-        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml"))
-
-      readerWithoutPackages shouldBe readerWithPackages
     }
   }
 }
