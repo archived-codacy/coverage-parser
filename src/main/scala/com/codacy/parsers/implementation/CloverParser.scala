4bdd3fd8c64ad4edddb60aa3d0f0ba0779b7adc1
Francisco <francisco@codacy.com>
2020-01-31 18:31:44 +0000

    clean: Improve code readability
    Change constants names to be more descriptive
    Extract strings to constants
    Replace (_ \ "@...").text by _ \@ "..."

--- src/main/scala/com/codacy/parsers/implementation/CloverParser.scala
+++ src/main/scala/com/codacy/parsers/implementation/CloverParser.scala
@@ -10,33 +10,33 @@ import scala.util.{Failure, Success, Try}
 import scala.xml.{Elem, Node, NodeSeq}
 
 object CloverParser extends CoverageParser {
-  private val COVERAGE = "coverage"
-  private val PROJECT = "project"
-  private val METRICS = "metrics"
+  private val CoverageTag = "coverage"
+  private val ProjectTag = "project"
+  private val MetricsTag = "metrics"
 
   override val name: String = "Clover"
 
   override def parse(rootProject: File, reportFile: File): Either[String, CoverageReport] = {
     val report = Try(XMLoader.loadFile(reportFile)) match {
       case Success(xml) if hasCorrectSchema(xml) =>
-        Right(xml \\ COVERAGE)
+        Right(xml \\ CoverageTag)
 
       case Success(_) =>
-        Left(s"Invalid report. Could not find tag hierarchy <$COVERAGE> <$PROJECT> <$METRICS> tags.")
+        Left(s"Invalid report. Could not find tag hierarchy <$CoverageTag> <$ProjectTag> <$MetricsTag> tags.")
 
       case Failure(ex) =>
         Left(s"Unparseable report. ${ex.getMessage}")
     }
 
-    report.right.flatMap(parse(rootProject, _))
+    report.right.map(parse(rootProject, _))
   }
 
   private def hasCorrectSchema(xml: Elem) =
-    (xml \\ COVERAGE \ PROJECT \ METRICS).nonEmpty
+    (xml \\ CoverageTag \ ProjectTag \ MetricsTag).nonEmpty
 
-  private def parse(rootProject: File, report: NodeSeq): Either[String, CoverageReport] = {
+  private def parse(rootProject: File, report: NodeSeq): CoverageReport = {
     // global metrics
-    val metrics = report \ PROJECT \ METRICS
+    val metrics = report \ ProjectTag \ MetricsTag
     val totalCoverage = getCoveragePercentage(metrics)
 
     val rootPath = TextUtils.sanitiseFilename(rootProject.getAbsolutePath)
@@ -44,27 +44,27 @@ object CloverParser extends CoverageParser {
       getCoverageFileReport(rootPath, f)
     }
 
-    Right(CoverageReport(totalCoverage, coverageFiles))
+    CoverageReport(totalCoverage, coverageFiles)
   }
 
   private def getCoveragePercentage(metrics: NodeSeq) = {
-    val totalStatements = TextUtils.asFloat((metrics \ "@statements").text)
-    val coveredStatements = TextUtils.asFloat((metrics \ "@coveredstatements").text)
+    val totalStatements = TextUtils.asFloat(metrics \@ "statements")
+    val coveredStatements = TextUtils.asFloat(metrics \@ "coveredstatements")
     val totalCoverage = if (totalStatements != 0) (coveredStatements / totalStatements) * 100 else 0
     scala.math.round(totalCoverage)
   }
 
   private def getCoverageFileReport(rootPath: String, fileNode: Node) = {
-    val filename = (fileNode \ "@name").text
+    val filename = fileNode \@ "name"
     val cleanFileName = TextUtils.sanitiseFilename(filename).stripPrefix(rootPath).stripPrefix("/")
 
-    val metrics = fileNode \ METRICS
+    val metrics = fileNode \ MetricsTag
     val fileCoverage = getCoveragePercentage(metrics)
 
     val lineCoverage = (for {
       line <- fileNode \ "line"
-      if (line \ "@type").text == "stmt"
-    } yield (line \ "@num").text.toInt -> (line \ "@count").text.toInt).toMap
+      if (line \@ "type") == "stmt"
+    } yield (line \@ "num").toInt -> (line \@ "count").toInt).toMap
 
     CoverageFileReport(cleanFileName, fileCoverage, lineCoverage)
   }
--- src/test/scala/com/codacy/parsers/CloverParserTest.scala
+++ src/test/scala/com/codacy/parsers/CloverParserTest.scala
@@ -6,17 +6,21 @@ import com.codacy.parsers.implementation.CloverParser
 import org.scalatest.{BeforeAndAfterAll, EitherValues, Matchers, WordSpec}
 
 class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers with EitherValues {
+  private val CoberturaReportPath = "src/test/resources/test_cobertura.xml"
+  private val NonExistentReportPath = "src/test/resources/non-existent.xml"
+  private val CloverReportPath = "src/test/resources/test_clover.xml"
+  private val CloverWithoutPackagesFilePath = "src/test/resources/test_clover_without_packages.xml"
   "parse" should {
     "identify report as invalid" when {
       "file does not follow the Cobertura format" in {
         // use some coverage file that does not follow the Clover format
-        val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_cobertura.xml"))
+        val reader = CloverParser.parse(new File("."), new File(CoberturaReportPath))
 
         reader shouldBe 'left
       }
 
       "file does not exist" in {
-        val reader = CloverParser.parse(new File("."), new File("src/test/resources/non-existent.xml"))
+        val reader = CloverParser.parse(new File("."), new File(NonExistentReportPath))
 
         reader shouldBe 'left
       }
@@ -24,13 +28,13 @@ class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers wit
 
     "identify the report as valid" when {
       "report has packages" in {
-        val reader = CloverParser.parse(new File("."), new File("src/test/resources/test_clover.xml"))
+        val reader = CloverParser.parse(new File("."), new File(CloverReportPath))
         reader shouldBe 'right
       }
 
       "report does not have packages" in {
         val readerWithoutPackages = CloverParser
-          .parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover_without_packages.xml"))
+          .parse(new File("/home/codacy-php/"), new File(CloverWithoutPackagesFilePath))
 
         readerWithoutPackages shouldBe 'right
       }
@@ -38,29 +42,29 @@ class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers wit
 
     "return the same report with or without packages" in {
       val readerWithoutPackages = CloverParser
-        .parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover_without_packages.xml"))
+        .parse(new File("/home/codacy-php/"), new File(CloverWithoutPackagesFilePath))
 
       val readerWithPackages =
-        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml"))
+        CloverParser.parse(new File("/home/codacy-php/"), new File(CloverReportPath))
 
       readerWithoutPackages shouldBe readerWithPackages
     }
 
     "return a report with the expected number of files" in {
       val report =
-        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+        CloverParser.parse(new File("/home/codacy-php/"), new File(CloverReportPath)).right.value
       report.fileReports should have length 5
     }
 
     "return a report with the expected total coverage" in {
       val report =
-        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+        CloverParser.parse(new File("/home/codacy-php/"), new File(CloverReportPath)).right.value
       report.total shouldBe 38
     }
 
     "return a report with the expected file names" in {
       val report =
-        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+        CloverParser.parse(new File("/home/codacy-php/"), new File(CloverReportPath)).right.value
       report.fileReports.map(_.filename).sorted shouldBe Seq(
         "src/Codacy/Coverage/Parser/Parser.php",
         "src/Codacy/Coverage/Report/CoverageReport.php",
@@ -72,7 +76,7 @@ class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers wit
 
     "return a report with the expected file coverage" in {
       val report =
-        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+        CloverParser.parse(new File("/home/codacy-php/"), new File(CloverReportPath)).right.value
       // coverage percentage for file Parser.php
       val firstFileReport = report.fileReports.headOption.getOrElse(fail("file reports list is empty"))
       firstFileReport.total shouldBe 33
@@ -84,7 +88,7 @@ class CloverParserTest extends WordSpec with BeforeAndAfterAll with Matchers wit
 
     "return a report with the expected file line coverage" in {
       val report =
-        CloverParser.parse(new File("/home/codacy-php/"), new File("src/test/resources/test_clover.xml")).right.value
+        CloverParser.parse(new File("/home/codacy-php/"), new File(CloverReportPath)).right.value
       report.fileReports(1).coverage shouldBe Map(
         11 -> 1,
         12 -> 1,
