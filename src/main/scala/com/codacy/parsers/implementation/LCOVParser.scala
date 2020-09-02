fdcd4dde60ef638362282411d45ea49696ef1ffa
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2020-09-02 12:42:12 +0100

    fix: LCOV should fail to parse xml files

--- src/main/scala/com/codacy/parsers/implementation/LCOVParser.scala
+++ src/main/scala/com/codacy/parsers/implementation/LCOVParser.scala
@@ -4,7 +4,7 @@ import com.codacy.parsers.CoverageParser
 import com.codacy.api.{CoverageFileReport, CoverageReport}
 import java.io.File
 
-import com.codacy.parsers.util.MathUtils
+import com.codacy.parsers.util.{MathUtils, XMLoader}
 
 import scala.io.Source
 import scala.util.{Failure, Success, Try}
@@ -17,9 +17,11 @@ object LCOVParser extends CoverageParser {
 
   override def parse(rootProject: File, reportFile: File): Either[String, CoverageReport] = {
     val report = Try(Source.fromFile(reportFile)) match {
+      case Success(lines) if Try(XMLoader.loadFile(reportFile)).isSuccess =>
+        Left(s"The file is not in the lcov format but is an xml.")
       case Success(lines) =>
+        val xml = Try(XMLoader.loadFile(reportFile))
         Right(lines.getLines)
-
       case Failure(ex) =>
         Left(s"Can't load report file. ${ex.getMessage}")
     }
